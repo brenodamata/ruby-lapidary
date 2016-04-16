fcf5f78d9799effc4f6e6ec1d1b974b341ad83bb
brenodamata <bjrdamata@gmail.com>
2016-04-16 12:43:54 -0400

    🗝  Decode caesar cipher

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -15,11 +15,40 @@ def caesar_cipher input, shift
   shifted_positions.pack('C*')
 end
 
-# puts caesar_cipher("This is a crypted message: Get to the chopper!", 11)
-# => "Estd td l ncjaepo xpddlrp: Rpe ez esp nszaapc!"
+def frequency input
+  counts = {}
 
-puts "Input string:"
-string_to_encrypt = gets.chomp
-puts "Shift factor:"
-shift_factor = gets.chomp.to_i
-puts "Caesar Cipher: #{caesar_cipher(string_to_encrypt, shift_factor)}"
+  input.each_char do |char|
+    next if char == " "
+    counts[char] = 0 unless counts.include?(char)
+    counts[char] += 1
+  end
+  counts
+end
+
+# 101 is 'e' the most frequent letter in the english language
+# after descoverying the most frequent letter in the crypted message
+# we can apply this rule to find out the shift used to crypt the original message
+# knowing the original shift we can make a new shift to decrypt it (26 - original shift)
+def decode_shift char
+  26 - (char.unpack('C*')[0] - 101)
+end
+
+def decode_cipher input
+  # Get most frequent letter in the crypted message
+  key = frequency(input).sort_by { |letter,frequency| frequency }.reverse.first[0]
+  dshift = decode_shift key
+
+  caesar_cipher input, dshift
+end
+
+puts caesar_cipher("This is a crypted message: Get to the chopper!", 12)
+
+# Decoding without knowing the shift
+puts decode_cipher "Ftue ue m odkbfqp yqeemsq: Sqf fa ftq otabbqd!".downcase
+
+# puts "Input string:"
+# string_to_encrypt = gets.chomp
+# puts "Shift factor:"
+# shift_factor = gets.chomp.to_i
+# puts "#{caesar_cipher(string_to_encrypt, shift_factor)}"
